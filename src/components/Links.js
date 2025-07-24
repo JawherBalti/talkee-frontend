ceafb57616b28c537665342856ffc8526ebbb3ce
JawharBalti <jawher-balti@live.com>
2025-07-24 20:31:22 +0100

    quick links component

---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+const Links =() => {
+    return (
+    <div className="sidebar-footer">
+      <a href="#">Terms</a>
+      <a href="#">Privacy</a>
+      <a href="#">Cookies</a>
+      <span>© 2023 Talkee</span>
+    </div>
+    )
+}
+
+export default Links
